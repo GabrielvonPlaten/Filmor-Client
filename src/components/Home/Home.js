6d9752fa7897c7cb93e0a287085cc1ce8d9256e2
Gabriel von Platen <gabrielvonplaten@gmail.com>
2019-06-23 19:48:11 +0200

    Create static jumbotron

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './Home.sass';
 
+import BG_1 from '../../Styles/images/bg-1.jpg';
+
 const Home = () => {
 
   return (
-    <div className="app">
-      <h1 className="title">Hello Filmor!</h1>
+    <div className="landing-page">
+      <div className="jumbotron-container">
+        <div 
+          className="jumbotron" 
+          style={{backgroundImage: "url(" + BG_1 + ")"}}>
+          <div className="jumbotron-header">
+            <p className="jumbotron__rating">7.4</p>
+            <h1 className="jumbotron__title">Toy Story</h1>
+            <p className="jumbotron__release-date">2019-06-19</p>
+            <button className="btn btn--yellow jumbotron__btn">Read More</button>
+          </div>
+          <div className="jumbotron__gradient-shadow"></div>
+        </div>
+      </div>
+      <div className="section-separation">
+        <h2 className="section__title">Popular</h2>
+      </div>
     </div>
   )
 }
