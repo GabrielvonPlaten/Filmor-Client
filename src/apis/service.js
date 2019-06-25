61f5881256131940e54c6e72af135751fe51e3df
Gabriel von Platen <gabrielvonplaten@gmail.com>
2019-06-25 03:29:22 +0200

    Add comments

--- src/apis/service.js
+++ src/apis/service.js
@@ -3,6 +3,7 @@ const API_KEY = process.env.API_KEY;
 
 
 export default {
+  // Movies
   getPopularMovies() {
     let url = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
     return axios.get(url);
--- src/components/ScrollToTop/ScrollToTop.js
+++ src/components/ScrollToTop/ScrollToTop.js
@@ -1,6 +1,8 @@
 import { useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 
+// This route is used to scroll to the top during route change
+
 function ScrollToTop({ history }) {
   useEffect(() => {
     const unlisten = history.listen(() => {
