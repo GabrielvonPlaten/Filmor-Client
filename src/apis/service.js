5a9db528dad47699f08cdab3ba00e46f2ed18fca
Gabriel von Platen <gabrielvonplaten@gmail.com>
2019-06-27 23:15:50 +0200

    Search results no longer returns adult content

--- src/apis/service.js
+++ src/apis/service.js
@@ -38,7 +38,7 @@ export default {
 
   // Search Results
   getSearchResults(query) {
-    let url = `https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=true`;
+    let url = `https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`;
     return axios.get(url);
   }
 }
