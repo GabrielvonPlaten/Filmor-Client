4f24865d2661821311b5fd2815821dc5d7f9f16b
Gabriel von Platen <gabrielvonplaten@gmail.com>
2019-06-27 23:06:22 +0200

    Display people and movies

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,7 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Search.sass';
 
 import apiService from '../../apis/service'; 
+
+// Components
+import PeopleIcons from '../PeopleIcons/PeopleIcons';
+import Poster from '../Poster/Poster';
 
 const Search = () => {
   const [searchData, setSearchData] = useState([]);
@@ -10,11 +14,36 @@ const Search = () => {
     e.preventDefault();
     let value = e.target.elements.query.value;
     apiService.getSearchResults(value)
-      .then(res => setSearchData(res.data))
+      .then(res => {
+        setSearchData(res.data.results)
+      })
       .catch(err => console.log(err));
+      
+      console.log(searchData);
+  };
 
-    console.log(searchData);
-  }
+  let displayPerson = searchData.map((res, index) => {
+    if (res.media_type === "person")
+    return (
+      <PeopleIcons
+        className="search-item"
+        key={index} 
+        personData={ res } />
+    )
+  });
+
+  let displayMovies = searchData.map((res, index) => {
+    if (res.media_type === "movie")
+    return (
+      <Poster
+        className="search-item"
+        key={index} 
+        mediaData={ res }
+        mediaTitle={ res.title }
+        mediaRating={ res.vote_average } />
+    )
+  });
+  
 
   return (
     <div className="search-container">
@@ -28,6 +57,22 @@ const Search = () => {
           placeholder="Search..." />
         <button className="btn btn--blue">Search</button>
       </form>
+      <div className="search-results-container">
+        { displayPerson.length > 0 && <h2 className="search-title">People</h2> }
+        { displayPerson.length > 0 &&
+          <div className="search-movies">
+            { displayPerson }
+          </div>
+        }
+      </div>
+      <div className="search-results-container">
+        { displayMovies.length > 0 && <h2 className="search-title">Movies</h2> }
+        { displayMovies.length > 0 &&
+          <div className="search-movies">
+            { displayMovies }
+          </div>
+        }
+      </div>
     </div>
   )
 }
