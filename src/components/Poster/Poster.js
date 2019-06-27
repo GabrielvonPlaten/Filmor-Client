a942f5f852a6c7690bb53d84a167a55c163e5fdc
Gabriel von Platen <gabrielvonplaten@gmail.com>
2019-06-27 23:09:20 +0200

    Add alternative image for movies

--- src/components/Poster/Poster.js
+++ src/components/Poster/Poster.js
@@ -4,11 +4,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 import PropTypes from 'prop-types';
 
+import noImage from '../../Styles/images/no-image.svg';
+
 const Poster = ({ mediaData, mediaTitle, mediaRating }) => {
+
+
   return (
     <div className="popular-movie">
       <img
-        src={"https://image.tmdb.org/t/p/original" + mediaData.poster_path}
+        src={mediaData.poster_path ? "https://image.tmdb.org/t/p/original" + mediaData.poster_path : noImage}
         className="movie__backdrop" />
       <h2 className="movie-poster__title">{mediaTitle}</h2>
       <div className="poster-rating">
