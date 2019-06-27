589b1a69f3c667c629c8dc0792d61c4fe4d4f7e8
Gabriel von Platen <gabrielvonplaten@gmail.com>
2019-06-27 22:25:56 +0200

    Fetch results data

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Search.sass';
 
+import apiService from '../../apis/service'; 
+
 const Search = () => {
+  const [searchData, setSearchData] = useState([]);
+
+  const searchMedia = (e) => {
+    e.preventDefault();
+    let value = e.target.elements.query.value;
+    apiService.getSearchResults(value)
+      .then(res => setSearchData(res.data))
+      .catch(err => console.log(err));
+
+    console.log(searchData);
+  }
+
   return (
     <div className="search-container">
-      <h2>Search</h2>
+      <h2 className="search-container__title">Search Any Media</h2>
+      <form
+        className="search-form"
+        onSubmit={searchMedia}>
+        <input 
+          className="search-form__input"
+          name="query" 
+          placeholder="Search..." />
+        <button className="btn btn--blue">Search</button>
+      </form>
     </div>
   )
 }
