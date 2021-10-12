0c0d5a67df664281fc2f0cb254e9d5f8c901aa3d
Gabriel von Platen <gabrielvonplaten@gmail.com>
2021-10-12 23:06:10 +0200

    Display list of all videos related to a movie in Video view

--- src/Views/Video/Video.tsx
+++ src/Views/Video/Video.tsx
@@ -5,14 +5,17 @@ import './Video.sass';
 // APIS
 import { getMovieData, getMovieVideo } from '../../apis/moviesService';
 import { MatchIdInterface } from '../../types/MatchInterface';
+import { Link } from 'react-router-dom';
 
 const Video: React.FC<{ match: MatchIdInterface }> = ({ match }) => {
   const [videoData, setVideoData] = useState<any>([]);
   const [movieData, setMovieData] = useState<any>([]);
+  const [videoId, setVideoId] = useState<string>('')
 
+  // Youtube options
   const opts: any = {
-    height: '468',
-    width: '768',
+    height: '100%',
+    width: '100%',
     playerVars: {
       // https://developers.google.com/youtube/player_parameters
       autoplay: 0,
@@ -29,6 +32,9 @@ const Video: React.FC<{ match: MatchIdInterface }> = ({ match }) => {
 
     setMovieData(movieResponse);
     setVideoData(videoResponse);
+
+    // Sets a video in the video player on component mounted
+    setVideoId(videoResponse.results[0].key);
   };
 
   const _onReady = (event: any) => {
@@ -36,27 +42,42 @@ const Video: React.FC<{ match: MatchIdInterface }> = ({ match }) => {
     event.target.pauseVideo();
   };
 
-  const videoKey = videoData?.results;
-  console.log(videoKey && videoKey[0]);
+  const videoArray = videoData?.results;
+  
+  const changeVideo = (key: string) => {
+    setVideoId(key)
+  }
   
 
   return (
-    <div className='video'>
-      <div
-        className='poster-background'
-        style={{
-          backgroundImage: `url(https://image.tmdb.org/t/p/original${movieData.backdrop_path})`,
-        }}
-      ></div>
-      <div className='video-container'>
-        <div className='video-header'>
-          <img src={`https://image.tmdb.org/t/p/original${movieData.poster_path}`} />
-          <YouTube
-            videoId={videoKey && videoKey[0].key}
-            opts={opts}
-            onReady={_onReady}
-          />
-        </div>
+    <div className='video' style={{
+      backgroundImage: `url(https://image.tmdb.org/t/p/original${movieData.backdrop_path})`,
+    }}>
+        <div className='video-container'>
+          <Link to={`/movie/${movieData.id}`}>
+            <h1 className="media-video-description__title">{movieData.title}</h1>
+          </Link>
+          <div className='video-header'>
+            <img src={`https://image.tmdb.org/t/p/original${movieData.poster_path}`} />
+            <YouTube
+              videoId={videoId}
+              opts={opts}
+              onReady={_onReady}
+            />
+          </div>
+          <div className="media-video-description">
+            <div className="media-video-list">
+              <h2 className="media-video-list__title">| Similar Videos</h2>
+              <ul>
+                {videoArray && videoArray?.map((item: any) => (
+                  <li key={item.id} onClick={() => changeVideo(item.key)}>
+                    <img src={`https://img.youtube.com/vi/${item.key}/0.jpg`}/>
+                    <p className={`media-video-list__video-title ${item.key === videoId ? 'video-selected' : ''}`}>{item.name}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
       </div>
     </div>
   );
